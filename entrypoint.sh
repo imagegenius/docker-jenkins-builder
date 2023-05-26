0787cb3a630cba8934fe61f0c995c132808b6737
hydazz <53986978+hydazz@users.noreply.github.com>
2023-05-26 17:52:21 +1000

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
 if [[ -z "${CI_RUN}" ]]; then
     cd /ansible
     ansible-playbook generate.yml ${EXTRA_PLAYBOOK_PARAM}
 else
-    echo '[ls.io-init] done.'
+    echo '[ig-init] done.'
     python3 /ansible/server.py
 fi
